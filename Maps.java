db3f9020399379dd8e086488e44e5b2cfa6cb977
Suraj182004 <surajyaligar2004@gmail.com>
2025-01-28 12:00:00 +0530

    Completed java starter

--- Maps.java
+++ Maps.java
@@ -12,7 +12,14 @@ public class Maps {
         // for (int i = 0; i < arr1.length; i++){
         //     System.out.print(arr1[i] + " ");
         // }
-
-
+        System.out.println(map.get("D"));
+        //learn optional
+        Optional<Integer> optionalValue = Optional.ofNullable(map.get("D"));
+        if (optionalValue.isPresent()) {
+            System.out.println(optionalValue.get());
+        }
+        else {
+            System.out.println("Value not present");
+        }
     }
 }
