2eba28f56770618e42af0ed42aa717e46f5729b4
Suraj182004 <surajyaligar2004@gmail.com>
2025-04-28 20:35:51 +0530

    Java prepation

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test{
+    int x=10;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a=1;
+        System.out.println(Integer.toString(a));//ans
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+
+    }
+}
